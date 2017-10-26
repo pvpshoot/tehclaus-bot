e5bd4c139968e7ded97c5b70ef9a6f3623c0fa1d
Vadim Marin <vadimmarin@Vadims-MacBook-Pro.local>
2017-10-26 17:26:31 +0400

    initial commit

---
+++ index.js
@@ -0,0 +1,43 @@
+const TelegramBot = require('node-telegram-bot-api');
+
+const token = '';
+
+const bot = new TelegramBot(token, { polling: true });
+
+const reactions = [
+    'Да.',
+    'Нет.',
+    'Xорошо.',
+    'Плохо.',
+    'Очень хорошо.',
+    'Очень плохо.',
+    'Смешно.',
+    'Не смешно.',
+    'Очень cмешно.',
+    'Бoян.',
+    'Не все видели.',
+    "Урбана ответ.",
+    'Очень не смешно.'
+];
+
+function shouldISendMessage(msg) {
+    const currentTimestamp = Math.floor(Date.now() / 1000);
+    const isFresh = currentTimestamp - msg.date < (1 * 60);
+    const messageHasPhoto = Boolean(msg.photo)
+
+    return isFresh && (messageHasPhoto);
+    // return isFresh && (messageHasPhoto || msg.from.username === 'urbanlich');
+}
+
+bot.on('message', function(msg) {
+    const chatId = msg.chat.id;
+    console.dir(msg)
+
+    if (shouldISendMessage(msg)) {
+        const message = reactions[Math.floor(Math.random() * reactions.length)];
+
+        setTimeout(function() {
+            bot.sendMessage(chatId, message);
+        }, 2000);
+    }
+});
